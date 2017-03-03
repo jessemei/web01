ca8a52d270b8ecd150eb2f3f99abfe3069c5dae1
jesse <jesse_mei@163.com>
2017-03-03 15:47:18 +0800

    分支2提交

---
+++ templates/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by meixiaohui on 2017/1/5.
+ */
+var  di = document.getElementsByClassName('divframe');
+var  du = document.getElementById('test1');
+var  a = di.innerHTML;
+alert('win size'+window.innerWidth+window.innerHeight) /**获取浏览器当前高宽*/
+alert(window.outerHeight)/** 可以获取分辨率**/
+
+console.log(du);
